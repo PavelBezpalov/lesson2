f1f184b11fb9924e93e12bdcb3fe91c11962ae9e
Pavel Bezpalov <pavel.bezpalov@cyber-wizard.com>
2015-10-16 15:21:14 +0300

    Refactor code

--- ball.rb
+++ ball.rb
@@ -3,21 +3,15 @@ require 'yaml'
 class Ball
   ANSWERS = YAML.load_file(File.join(__dir__, './answers.yml'))
 
-  def answers_with_color
-    answers = []
-    answers.push(color_code: 31, messages: ANSWERS[0..4])
-    answers.push(color_code: 32, messages: ANSWERS[5..9]) if ANSWERS.size > 5
-    answers.push(color_code: 33, messages: ANSWERS[10..14]) if
-    ANSWERS.size > 10
-    answers.push(color_code: 34, messages: ANSWERS[15..19]) if
-    ANSWERS.size > 15
-    answers
+  def color(answer)
+    [4, 9, 14, 19].inject(35) do |color, value|
+      color - ((0..value).cover?(ANSWERS.index(answer)) ? 1 : 0)
+    end
   end
 
   def shake
-    answer = answers_with_color.sample
-    message = answer[:messages].sample
-    puts "\e[#{answer[:color_code]}m#{message}\e[0m"
-    message
+    answer = ANSWERS.sample
+    puts "\e[#{color(answer)}m#{answer}\e[0m"
+    answer
   end
 end
